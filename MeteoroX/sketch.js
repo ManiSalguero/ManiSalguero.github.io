bd1f69a3356ecb3d542fb6a8e8d60c806cede12f
ManiSalguero <mnsalguero@udlanet.ec>
2016-05-26 12:14:30 -0500

    c

--- MeteoroX/sketch.js
+++ MeteoroX/sketch.js
@@ -1,4 +1,3 @@
-
 var GRAVITY = 0;
 var GROUND_Y = 950;
 var MIN_OPENING = 300;
@@ -8,7 +7,7 @@ var mets;
 var gameOver;
 
 var naveImg, metImg, groundImg, bgImg;
-
+var postexto = 0;
 
 function setup() {
   createCanvas(800, 800);
@@ -19,25 +18,25 @@ function setup() {
   bgImg = loadImage("assets/fondo.png");
   metImg = loadImage("assets/met.png");
 
-  nave = createSprite(width*.5, height*.5, 40, 40);
+  nave = createSprite(width * .5, height * .5, 40, 40);
   nave.rotateToDirection = false;
-  
+
   nave.velocity.x = 10;
-  
+
   nave.setCollider("rectangle", 20, -12, 100, 50);
-  nave.debug=true;
+  nave.debug = true;
   nave.addImage(naveImg);
 
   ground = createSprite(800 / 2); //image 800x200
   ground.addImage(groundImg);
 
- 
+  postexto = 0.9 * width;
 
   mets = new Group();
-  
-  
-  
-  
+
+
+
+
   gameOver = true;
   updateSprites(false);
 
@@ -56,8 +55,8 @@ function draw() {
 
     nave.velocity.y += GRAVITY;
 
-    
-      nave.position.y = mouseY;
+
+    nave.position.y = mouseY;
 
     if (nave.position.y + nave.height / 2 > GROUND_Y)
       die();
@@ -72,13 +71,13 @@ function draw() {
       var metH = random(850, 2000);
       var met = createSprite(nave.position.x + width, GROUND_Y - metH / 2 + 1 + 100, 80, metH);
       met.addImage(metImg);
-      met.scale= random(0,1.5);
+      met.scale = random(0, 1.5);
       met.duplicate
-      met.velocity.x=random(2,10);
-      met.rotate= random(0,360);
+      met.velocity.x = random(5, 13);
+      met.rotate = random(0, 360);
       mets.add(met);
-met.setCollider("circle", 0, 0, 35);
-  met.debug=true;
+      met.setCollider("circle", 0, 0, 35);
+      met.debug = true;
 
 
       //top pipe
@@ -99,27 +98,28 @@ met.setCollider("circle", 0, 0, 35);
 
   camera.position.x = nave.position.x + width / 4;
 
-  
-  if (camera.position.x)
-    
 
-  background(98,105,124);
+  if (camera.position.x)
+
+
+    background(98, 105, 124);
   camera.off();
   image(bgImg, 0);
   camera.on();
-
   drawSprites(mets);
   drawSprite(nave);
- 
+  text(millis()/1000, postexto, 0.1 * height);
+  postexto = nave.position.x+0.5*width;
 
- 
+
 }
 
 function die() {
   updateSprites(false);
   gameOver = true;
   
-  
+
+
 }
 
 function newGame() {
@@ -136,5 +136,7 @@ function newGame() {
 function mousePressed() {
   if (gameOver)
     newGame();
-  
+      
+
+
 }
